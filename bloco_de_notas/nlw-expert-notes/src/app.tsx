5b1d3d4b8041831284cc36a81881aaa3fdf12c2a
Gabriella Oliveira <gabriellaszaolv50@gmail.com>
2024-02-06 21:32:28 -0300

    alterações da segunda aula

--- bloco_de_notas/nlw-expert-notes/src/app.tsx
+++ bloco_de_notas/nlw-expert-notes/src/app.tsx
@@ -1,9 +1,16 @@
 import logo from './assets/logo-nlw-expert.svg'
+import NewNoteCard from './components/new-note-card'
+import { NoteCard } from './components/note-card'
+
 
 export default function App() {
 
+  const note ={
+    date: new Date(),
+    content: 'Hello World'
+  }
   return( 
-    <div className='mx-auto max-w-6xl my-12'>
+    <div className='mx-auto max-w-6xl my-12 space-y-6'>
       <img src={logo} alt='NWL EXPERT'/>
 
       <form className='w-full space-y-6'>
@@ -17,19 +24,9 @@ export default function App() {
       <div className='h-px bg-slate-700' />
         <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
 
-          <div className='rounded-md bg-slate-700'>
-              <span>Adicionar nota</span>
-              <p>Grave uma nota em áudio que será convertida para texto automaticamente</p>
-          </div>
-
-          <div className='rounded-md bg-slate-800'>
-
-          </div>
-
-          <div className='rounded-md bg-slate-800'>
-
-  
-        </div>
+          <NewNoteCard />
+          <NoteCard note={note}/>
+        
       </div>
     </div>
    )
